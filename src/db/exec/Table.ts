82cd6be5de40edc59fc66c60cfcc429cf40b6511
Peter M. Fischer <peter.fischer@informatik.uni-augsburg.de>
2021-09-18 16:48:29 +0200

    Use relative imports (as absolute imports don't work well on the backend)

--- src/db/exec/Table.ts
+++ src/db/exec/Table.ts
@@ -4,7 +4,7 @@
 * License, v. 2.0. If a copy of the MPL was not distributed with this
 * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { printValue } from 'db/exec/ValueExpr';
+import { printValue } from './ValueExpr';
 import { Relation } from './Relation';
 import { Data, DataType, Schema } from './Schema';
 
